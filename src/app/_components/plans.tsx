d29e822c1367dda7a82ce26cc839465a50f95c00
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2024-06-09 01:57:41 +0100

    remove node env

--- src/app/_components/plans.tsx
+++ src/app/_components/plans.tsx
@@ -2,14 +2,8 @@ export const plans = [
   {
     name: "Basic",
     description: "Perfect for individuals and small teams.",
-    link:
-      process.env.NODE_ENV === "development"
-        ? "https://buy.stripe.com/test_3cs9Dg9F5gV09ywcMM"
-        : "",
-    priceId:
-      process.env.NODE_ENV === "development"
-        ? "price_1PKQBIRwYtrOJacvkwGQeU5m"
-        : "",
+    link: "https://buy.stripe.com/test_3cs9Dg9F5gV09ywcMM",
+    priceId: "price_1PKQBIRwYtrOJacvkwGQeU5m",
     price: 9,
     duration: "/month",
   },
@@ -17,14 +11,10 @@ export const plans = [
     name: "Pro",
     description: "Ideal for growing teams and small businesses.",
 
-    link:
-      process.env.NODE_ENV === "development"
-        ? "https://buy.stripe.com/test_aEU16K8B15cibGEaEG"
-        : "",
-    priceId:
-      process.env.NODE_ENV === "development"
-        ? "price_1PKR6KRwYtrOJacvspFeAFsn"
-        : "",
+    link: "https://buy.stripe.com/test_aEU16K8B15cibGEaEG",
+
+    priceId: "price_1PKR6KRwYtrOJacvspFeAFsn",
+
     price: 29,
     duration: "/month",
   },
@@ -32,14 +22,10 @@ export const plans = [
     name: "Enterprise",
     description: "Tailored for large teams and organizations.",
 
-    link:
-      process.env.NODE_ENV === "development"
-        ? "https://buy.stripe.com/test_00gbLo8B17kq5ig6op"
-        : "",
-    priceId:
-      process.env.NODE_ENV === "development"
-        ? "price_1PKR6fRwYtrOJacvC9eEa9xJ"
-        : "",
+    link: "https://buy.stripe.com/test_00gbLo8B17kq5ig6op",
+
+    priceId: "price_1PKR6fRwYtrOJacvC9eEa9xJ",
+
     price: 99,
     duration: "/month",
   },
